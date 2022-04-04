d496d7021e74c54d3f12eb414d8137ef3cd5da9d
blackbat13 <blackbat13@gmail.com>
2022-04-04 23:49:06 +0200

    Project setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PainterLuaLove
  
+# Assets
+
+[kenney.nl](https://kenney.nl)
---
+++ main.lua
@@ -0,0 +1,118 @@
+function love.load()
+
+    Const = {width = 1000, height = 1000, margin = 50, framerate = 60}
+
+    ColorCanvas = love.graphics.newCanvas(Const.width, Const.height)
+    love.graphics.setCanvas(ColorCanvas)
+    love.graphics.clear()
+    love.graphics.setCanvas()
+
+    Blue = {
+        x = Const.margin,
+        y = Const.margin,
+        angle = 0,
+        drawable = love.graphics.newImage("images/blue.png"),
+        name = "Blue",
+        color = {red = 33, green = 132, blue = 211},
+        keys = {left = "q", right = "e", power = "w"},
+        velocity = 10,
+        radius = 20,
+        power = false,
+        powerTimeour = 45,
+        percent = 0,
+        pixels = 0
+    }
+
+    Red = {
+        x = Const.width - Const.margin,
+        y = Const.margin,
+        angle = 0,
+        drawable = love.graphics.newImage("images/red.png"),
+        name = "Blue",
+        color = {red = 221, green = 78, blue = 84},
+        keys = {left = "i", right = "p", power = "o"},
+        velocity = 20,
+        radius = 12,
+        power = false,
+        powerTimeour = 5,
+        percent = 0,
+        pixels = 0
+    }
+
+    Green = {
+        x = Const.margin,
+        y = Const.height - Const.margin,
+        angle = 0,
+        drawable = love.graphics.newImage("images/green.png"),
+        name = "Blue",
+        color = {red = 73, green = 180, blue = 126},
+        keys = {left = "left", right = "right", power = "up"},
+        velocity = 10,
+        radius = 20,
+        power = false,
+        powerTimeour = 30,
+        percent = 0,
+        pixels = 0
+    }
+
+    Grey = {
+        x = Const.width - Const.margin,
+        y = Const.height - Const.margin,
+        angle = 0,
+        drawable = love.graphics.newImage("images/grey.png"),
+        name = "Blue",
+        color = {red = 147, green = 127, blue = 124},
+        keys = {left = "kp7", right = "kp9", power = "kp8"},
+        velocity = 5,
+        radius = 40,
+        power = false,
+        powerTimeour = 15,
+        percent = 0,
+        pixels = 0
+    }
+
+    Players = {Blue, Green, Red, Grey}
+
+end
+
+function love.draw()
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.setBlendMode("alpha", "premultiplied")
+    love.graphics.draw(ColorCanvas)
+    DrawPlayers()
+end
+
+function DrawPlayers()
+    for i = 1, #(Players) do
+        love.graphics.draw(Players[i].drawable, Players[i].x, Players[i].y, math.rad(Players[i].angle), 1, 1, 20, 20)
+    end
+end
+
+function love.update(dt)
+    for i = 1, #(Players) do
+        UpdatePlayer(Players[i], dt)
+    end
+end
+
+function UpdatePlayer(player, dt)
+    if love.keyboard.isDown(player.keys.left) then
+        player.angle = (player.angle + player.velocity / 2) % 360
+    end
+    if love.keyboard.isDown(player.keys.right) then
+        player.angle = (player.angle - player.velocity / 2) % 360
+    end
+
+    player.x = player.x + math.sin(math.rad(player.angle + 90)) * player.velocity * dt * Const.framerate
+    player.y = player.y + math.cos(math.rad(player.angle + 90)) * player.velocity * dt * Const.framerate
+
+    if player.x < 0 or player.x >= Const.width or player.y < 0 or player.y >= Const.height then
+        player.x = player.x - math.sin(math.rad(player.angle + 90)) * player.velocity * dt * Const.framerate
+        player.y = player.y - math.cos(math.rad(player.angle + 90)) * player.velocity * dt * Const.framerate
+        player.angle = (player.angle + math.random(100, 250)) % 360
+    end
+
+    love.graphics.setCanvas(ColorCanvas)
+    love.graphics.setColor(love.math.colorFromBytes(player.color.red, player.color.green, player.color.blue))
+    love.graphics.circle("fill", player.x, player.y, player.radius)
+    love.graphics.setCanvas()
+end
